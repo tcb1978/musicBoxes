fcaed4445aee202fade017c68f8499304cb55b23
Matthew Eldredge <mattheweldredge@gmail.com>
2017-07-27 17:37:58 -0700

    index.php: include audio files/ move audio files to src folder

--- index.php
+++ index.php
@@ -14,8 +14,53 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
         <link rel="stylesheet" href="src/css/main.css">
     </head>
+    <title>Music Boxes</title>
     <body>
+        <audio controls>
+            <source src="/src/music_notes/a_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/a_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/a_note.wav" type="audio/wav">
+        </audio>
+        <audio controls>
+            <source src="/src/music_notes/b_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/b_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/b_note.wav" type="audio/wav">
+        </audio>
+        <audio controls>
+            <source src="/src/music_notes/e_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/c_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/c_note.wav" type="audio/wav">
+        </audio>
+        <audio controls>
+            <source src="/src/music_notes/d_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/d_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/d_note.wav" type="audio/wav">
+        </audio>
+        <audio controls>
+            <source src="/src/music_notes/e_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/e_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/e_note.wav" type="audio/wav">
+        </audio>
+        <audio controls>
+            <source src="/src/music_notes/f_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/f_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/f_note.wav" type="audio/wav">
+        </audio>
+        <audio controls>
+            <source src="/src/music_notes/g_note.mp3" type="audio/mpeg">
+            <source src="/src/music_notes/g_note.ogg" type="audio/ogg">
+            <source src="/src/music_notes/g_note.wav" type="audio/wav">
+        </audio>
         
+        <div id="instrument">
+            <div id="c" class="box"></div>
+            <div id="d" class="box"></div>
+            <div id="e" class="box"></div>
+            <div id="f" class="box"></div>
+            <div id="g" class="box"></div>
+            <div id="a" class="box"></div>
+            <div id="b" class="box"></div>
+        </div>
         
 
         <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
